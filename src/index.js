19e6c9b298bf0c3378b2981bdaa2ffb2046e07a8
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-03-26 19:57:49 -0300

    teste

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+
+function App () {
+
+
+    <div>
+        <h1>Teste</h1>
+    </div>
+
+
+}
+
+export default App;
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+   )
