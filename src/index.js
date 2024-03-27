8162a13879bf9f7bc55059e76d4139c5fc352b8b
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-03-26 23:36:57 -0300

    entrega01(front): cria a aplicacao React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 
 const App = () => {
 
@@ -11,7 +11,6 @@ const App = () => {
     )
 
 }
-
 ReactDOM.render(
     <App />,
     document.querySelector("#root")
